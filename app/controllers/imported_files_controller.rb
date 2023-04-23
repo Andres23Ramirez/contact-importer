60b71c188b4db42b7b1a84722bc950c38abc3f89
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-23 10:49:22 -0500

    Feat: add contact import job and matching column tool

--- app/controllers/imported_files_controller.rb
+++ app/controllers/imported_files_controller.rb
@@ -12,8 +12,16 @@ class ImportedFilesController < ApplicationController
   def create
     @imported_file = ImportedFile.new(name: imported_file_params[:file].original_filename, user: current_user)
 
+    file = imported_file_params[:file]
+
     if @imported_file.save!
-      # Procesar el archivo CSV
+      file_path = Rails.root.join('tmp', file.original_filename)
+      File.open(file_path, 'wb') do |f|
+        f.write(file.read)
+      end
+
+      ContactImportJob.perform_later(@imported_file.id, file_path.to_s, getArrayColumns(), current_user, imported_file_params[:headers])
+
       flash[:success] = "The file has been successfully uploaded."
       redirect_to root_path
     else
@@ -25,6 +33,13 @@ class ImportedFilesController < ApplicationController
   private
 
   def imported_file_params
-    params.permit(:file, :authenticity_token, :column_1, :column_2, :column_3, :column_4, :column_5, :column_6, :column_7 )
+    params.permit(:file, :authenticity_token, :column_1, :column_2, :column_3, :column_4, :column_5, :column_6, :column_7, :headers )
   end
+
+  def getArrayColumns
+    [imported_file_params[:column_1], imported_file_params[:column_2], imported_file_params[:column_3], 
+    imported_file_params[:column_4], imported_file_params[:column_5], imported_file_params[:column_6], 
+    imported_file_params[:column_7]]
+  end
+  
 end
---
+++ app/jobs/contact_import_job.rb
@@ -0,0 +1,38 @@
+class ContactImportJob < ApplicationJob
+  require 'csv'
+  queue_as :default
+
+  def perform(imported_file_id, file_path, columns, user, headers)
+    imported_file = ImportedFile.find(imported_file_id)
+    imported_file.status = 1
+    imported_file.save
+
+    failed = true
+
+    CSV.foreach(file_path, headers: !!headers) do |row|
+      contact = Contact.new(
+        name: row[columns.index("Name")],
+        date_of_birth: row[columns.index('Date of Birth')],
+        phone: row[columns.index('Phone')],
+        address: row[columns.index('Address')],
+        credit_card_number: row[columns.index('Credit Card Number')],
+        credit_card_network: row[columns.index('Credit Card Network')],
+        email: row[columns.index('Email')], 
+        user: user
+      )
+    
+      if contact.save
+        failed = false
+        puts "Contact Save: #{contact.inspect}"
+      else
+        puts "Error saving contact: #{contact.errors.full_messages.join(", ")}"
+      end
+    end
+
+    imported_file.status = failed ? 2 : 3
+    imported_file.save
+
+    # delete the temporary file
+    File.delete(file_path)
+  end
+end
